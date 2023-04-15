yaap/frameworks_base

SystemUI: CompassTile: Add a device supports compass check
- Previously it showed up on devices even without compass support
  making it a bloat

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
Change-Id: Iba3b29e4be2c0fd5f97573c2d0abbde7ad1db995

packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
@@ -28,6 +28,7 @@ import android.widget.ImageView;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.yaap.YaapUtils;
 import com.android.systemui.R;
 import com.android.systemui.plugins.qs.QSIconView;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
@@ -161,6 +162,11 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
         } else {
             return mContext.getString(R.string.accessibility_quick_settings_compass_off);
         }
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return YaapUtils.deviceHasCompass(mContext);
     }
 
     @Override

courbet-space/frameworks_base-1

Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
Signed-off-by: Aman Singh <amankumarsingh1412@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
@@ -23,6 +23,8 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import com.android.internal.util.derp.derpUtils;
+
 import android.os.Handler;
 import android.os.Looper;
 import android.service.quicksettings.Tile;
@@ -178,6 +180,11 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.DERP;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return derpUtils.deviceHasCompass(mContext);
     }
 
     @Override